333b214c21db81362c2f35e432e6a2d05cc26719
Aayush Buchamwandla <aayushbuchamwandla@cs.ucsb.edu>
2025-05-16 16:16:23 -0700

    y

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-// Feb 14: This file should implement the game using a custom implementation of a BST (that is based on your implementation from lab02)
+// This file should implement the game using a custom implementation of a BST (that is based on your implementation from lab02)
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -6,35 +6,54 @@
 #include "card_list.h"
 //Do not include set in this file
 
+
 using namespace std;
 
+
 int main(int argv, char** argc){
-  if(argv < 3){
-    cout << "Please provide 2 file names" << endl;
-    return 1;
-  }
-  
+ if(argv < 3){
+   cout << "Please provide 2 file names" << endl;
+   return 1;
+ }
   ifstream cardFile1 (argc[1]);
-  ifstream cardFile2 (argc[2]);
-  string line;
-
-  if (cardFile1.fail() || cardFile2.fail() ){
-    cout << "Could not open file " << argc[2];
-    return 1;
-  }
-
-  //Read each file
-  while (getline (cardFile1, line) && (line.length() > 0)){
-
-  }
-  cardFile1.close();
+ ifstream cardFile2 (argc[2]);
+ string line;
 
 
-  while (getline (cardFile2, line) && (line.length() > 0)){
+ if (cardFile1.fail() || cardFile2.fail() ){
+   cout << "Could not open file " << argc[2];
+   return 1;
+ }
 
-  }
-  cardFile2.close();
-  
-  
+
+ BST Alice;
+ BST Bob;
+
+
+ while (getline (cardFile1, line) && (line.length() > 0)){
+   char suit = line[0];
+   string rank = line.substr(2);
+   Alice.insert(Card(suit,rank));
+ }
+ cardFile1.close();
+
+
+
+
+ while (getline (cardFile2, line) && (line.length() > 0)){
+   char suit = line[0];
+   string rank = line.substr(2);
+   Bob.insert(Card(suit,rank));
+ }
+ cardFile2.close();
+ //   playGame(Alice,Bob);
+
+
+ cout<<"Alice's cards: "<<endl;
+ Alice.printDeck();
+
+
+ cout<<"Bob's cards: "<<endl;
+ Bob.printDeck();
   return 0;
 }
--- main_set.cpp
+++ main_set.cpp
@@ -1,40 +1,101 @@
-// Feb 14: This file should implement the game using the std::set container class
-// Do not include card_list.h in this file
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <set>
 #include "card.h"
 
+
 using namespace std;
 
+
 int main(int argv, char** argc){
-  if(argv < 3){
-    cout << "Please provide 2 file names" << endl;
-    return 1;
-  }
-  
+ if(argv < 3){
+   cout << "Please provide 2 file names" << endl;
+   return 1;
+ }
   ifstream cardFile1 (argc[1]);
-  ifstream cardFile2 (argc[2]);
-  string line;
-
-  if (cardFile1.fail() || cardFile2.fail() ){
-    cout << "Could not open file " << argc[2];
-    return 1;
-  }
-
-  //Read each file
-  while (getline (cardFile1, line) && (line.length() > 0)){
-
-  }
-  cardFile1.close();
+ ifstream cardFile2 (argc[2]);
+ string line;
 
 
-  while (getline (cardFile2, line) && (line.length() > 0)){
+ if (cardFile1.fail() || cardFile2.fail() ){
+   cout << "Could not open file " << argc[2];
+   return 1;
+ }
 
-  }
-  cardFile2.close();
-  
-  
-  return 0;
+
+ set<Card> Alice;
+ set<Card> Bob;
+
+
+ //Read each file
+ while (getline (cardFile1, line) && (line.length() > 0)){
+   char suit = line[0];
+   string rank = line.substr(2);
+   Card c(suit,rank);
+   Alice.insert(c);
+ }
+ cardFile1.close();
+
+
+
+
+ while (getline (cardFile2, line) && (line.length() > 0)){
+   char suit = line[0];
+   string rank = line.substr(2);
+   Card c(suit,rank);
+   Bob.insert(c);
+ }
+ cardFile2.close();
+  while (true) {
+   bool found = false;
+
+
+   for (const auto& A: Alice) {
+       if (Bob.count(A)) {
+           cout<<"Alice picked matching card "<<A<<endl;
+           Bob.erase(A);
+           Alice.erase(A);
+           found = true;
+           break;
+       }
+   }
+   if (found == false) {
+       break;
+   }
+
+
+   found = false;
+
+
+   auto i = Bob.rbegin();
+   while (i != Bob.rend()) {
+       if (Alice.count(*i)) {
+           cout<<"Bob picked matching card "<<*i<<endl;
+           found = true;
+           Alice.erase(*i);
+           Bob.erase(*i);
+           break;
+       }
+       ++i;
+   }
+   if (found == false) {
+       break;
+   }
+ }
+
+
+ cout<<"Alice's cards: "<<endl;
+ for (const auto& card: Alice) {
+   cout<<card<<endl;
+ }
+
+
+ cout<<"Bob's cards: "<<endl;
+ for (const auto& card: Bob) {
+   cout<<card<<endl;
+ }
+
+
+ return 0;
 }
